43b1d857fcd8a2b7bc17c999c233bf6cc8fc2f0e
NarminGahramanova <gahramanovanarmin02@gmail.com>
2025-05-16 18:00:23 +0400

    some changed

---
+++ JavaScript/script.js
@@ -0,0 +1,308 @@
+// deyiskenler
+
+
+//  var password;
+//   console.log(password)
+
+//  password = "narmin";
+//  console.log(password)
+
+// yas = 22;
+// console.log(yas)
+//  deyisken tanimlama qaydalari
+
+
+
+// var adSoyad = "narmin Gahramanova"
+// console.log(adSoyad)
+
+// const email = "1234@gmail.com"
+// console.log(email)
+
+
+
+// //deyisken type
+
+
+// //primitiv type
+
+// let firstName = "narmin"
+// console.log(typeof firstName)
+
+
+// //number
+
+// let age = 22
+// console.log(typeof age)
+
+// //boolean
+
+// let kredit = false
+// console.log(typeof kredit)
+
+
+// // underfined
+
+// let phone
+// console.log(typeof phone)
+
+
+
+// // referce of type (primitiv olmayan tipler)
+// // arrAY 
+
+// let list = ["a", "b", "c", "d"]
+// console.log(typeof list)
+
+// //objects literals
+
+// let adress = {
+//     city: "baki",
+//     country: "azerbaycan",
+
+// }
+// console.log(typeof adress)
+
+// var hesapla = function () {
+
+//     return 0;
+// }
+// console.log(typeof hesapla)
+
+
+
+// let veri;
+// const a = 20;
+// const b = 10;
+// let d = 4
+// const c = 10
+
+// veri = a + b;
+
+// veri = a - b;
+// veri = d++;
+// veri = d;
+
+
+// console.log(veri);
+// console.log(typeof veri);
+// console.log(d);
+
+
+
+
+
+// let veri;
+// const a = 20;
+// const b = 10;
+// veri = a;
+// veri = a;
+
+
+
+
+
+
+
+
+
+
+// veri = !(a>b)
+
+
+
+
+
+
+
+
+// console.log(veri);
+// console.log(typeof veri);
+
+
+
+
+
+
+
+
+
+
+
+// let zaman = new Date();
+
+
+//  let birthday = new Date(2002,11,1);
+
+
+// zaman.setMonth(3)
+
+// console.log(zaman.getMonth());
+// console.log(zaman.getDate());
+// console.log(zaman.getFullYear());
+// console.log(zaman.getDay());
+// console.log(zaman.getHours());
+// console.log(zaman.getSeconds());
+// console.log(zaman.getMinutes());
+
+
+//  let birthday = new Date(2003,02,7);
+// console.log(zaman.getFullYear() - birthday.getFullYear());
+// console.log(zaman.getMonth() - birthday.getMonth());
+
+
+
+
+// console.log(zaman);
+// console.log(typeof zaman);
+// */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let veri;
+
+
+// veri = Number("5");
+// veri = parseFloat("5");
+// console.log(veri);
+// console.log(typeof veri);
+
+
+
+
+
+
+// // veri = Number ("5");
+// // veri = parseInt ("5");
+// // veri = parseFloat  ("5.2");
+// // veri = parseInt ("C5B");
+
+
+// // veri = isNaN ("c5");
+// // veri = isNaN ("5");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var sayi = 12.34123452345;
+
+
+// veri = sayi.toPrecision(5);
+// veri = sayi.toFixed(4);
+
+//  veri = Math.PI;
+//  veri = Math.round(2.5);
+//  veri = Math.ceil(7.1)
+// veri = Math.floor(9.9)
+//  veri = Math.pow(9,2);
+// veri = Math.sqrt(9);
+// veri = Math.abs(-454);
+// veri = Math.min(1,4,5,8,2);
+// veri = Math.max(1,4,5,8,2);
+
+
+// veri = Math.floor(Math.random()*5+1);
+
+
+//  veri = Math.pow(9,2);
+
+//  console.log(veri);
+//  console.log(typeof veri);
+//  veri = Math.abs(-454);
+
+
+
+
+
+
+// const firstname = "Narmin";
+// const lastname = "Gahramanova";
+// const age = 22;
+
+// let veri;
+
+
+
+
+// veri = firstname + " " + lastname;
+// veri = "narmin"
+// veri += " gahramanova"
+// veri = " benim adim " + firstname + " " + lastname + "dir ve beimin " + age + "  yasim var"
+
+
+// // // veri =veri.toUpperCase();
+// // // veri = veri.toLocaleLowerCase()
+
+// // // // veri = veri.slice(1,4)
+
+
+// // veri = veri.indexOf("r")
+// // veri = veri.replace("narmin", "su")
+
+
+
+
+// console.log(veri);
+// console.log(typeof veri);
+
+
+
+//-------------------------------------task2-----------------------------------------
+
+//  const firstname = "Narmin";
+//  const lastname = "Gahramanova";
+//  const age = 22;
+
+// veri = firstname + " " + lastname;
+// veri = "narmin"
+// veri += " gahramanova"
+// veri = " benim adim " + firstname + " " + lastname + "dir ve beimin " + age + "  yasim var"
+// veri = veri.length
+
+// console.log(veri)
+// console.log(typeof veri)
+
+
+
+
+
+
+//-----------------------------------task 3------------------------//
+
+
+// function hesab(a, b) {
+//      if (a >= 10 && a <= 99 && b >= 10 && b <= 9)
+//               return ((a - b) / (a + b));
+//  }
+
+// console.log((hesab)(45, 15));
+
+
